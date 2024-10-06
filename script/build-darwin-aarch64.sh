7b3f266f029bc175310c8d4770b70cdd1ba78bb9
Cameron Dutro <camertron@gmail.com>
2024-10-06 15:01:52 -0700

    Oops, fix typo x86_64 -> aarch64

--- script/build-darwin-aarch64.sh
+++ script/build-darwin-aarch64.sh
@@ -6,6 +6,6 @@ set -eo pipefail
 cross build --target=aarch64-apple-darwin --release
 
 mkdir rscc-darwin-aarch64
-cp target/x86_64-apple-darwin/release/rscc rscc-darwin-aarch64/
+cp target/aarch64-apple-darwin/release/rscc rscc-darwin-aarch64/
 cp rsc.c rscc-darwin-aarch64/
 tar -czvf rscc-darwin-aarch64.tar.gz rscc-darwin-aarch64/
